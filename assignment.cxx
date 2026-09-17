5ec813c4ce589f1d96a8828b375bf5601c338d96
nw234024-design <nw234024@gmail.com>
2026-09-17 14:25:24 +0300

    Add files via upload

---
+++ assignment.cxx
@@ -0,0 +1,26 @@
+/*
+
+Name:Nancy Wambui Kabiri
+Reg no:CT100/G/30629/26
+Description:assignment week one
+Date:15/09/26
+
+*/
+
+#include<stdio.h>         //scan f(),print f()
+
+int main()    {
+    
+    print f( "introduction to programming.\n");
+    print f("Data structures.\n");
+    prinf f("computer networks.\n");
+    prinf f("Databases systems.\n");
+    prinf f("Software engeneering.");
+    
+}
+
+
+
+Return 0;//execution successful
+
+	
