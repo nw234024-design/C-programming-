1a0765cab0048cfb483a9ed9956627dd5678a78a
nw234024-design <nw234024@gmail.com>
2026-09-21 19:39:24 +0300

    Add files via upload

---
+++ assignment week one.cxx
@@ -0,0 +1,30 @@
+//variables and data types
+
+#include <stdio.h>
+
+int main() {
+    //declare variables
+    float height;//%f
+    double balance;//%lf
+    int phone;//%d    
+        
+        
+        
+    printf("Enter height in mm or cm\t");
+    scanf("%f", &height);
+    
+    printf("Enter balance\t");
+    scanf("%lf",&balance);
+    
+    printf("Enter phone number\t");
+    scanf("%d",&phone);
+    
+    
+    printf("The height is %f\n",height);
+    printf("The balance is %lf\n",balance);
+    printf("phone number is %d",phone);
+   
+    
+}
+    
+    
